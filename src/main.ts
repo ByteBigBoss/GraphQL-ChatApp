47e23b4bafb60dd3c7fa083ee24c52bb2dc2d32a
Nethmina Sandaruwan <125788788+ByteBigBoss@users.noreply.github.com>
2024-08-14 21:15:05 +0530

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = 8000;
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin:["http://localhost:3001"],
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
+  });
+ 
   await app.listen(port).then(()=>{
     console.log("App started listening on port: "+ port);
   });
+  
 }
 bootstrap();
